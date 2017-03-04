a5a838c7d9ea033309cbbb034bc6835cd8a14f6a
smmakowski <makowski.sm@gmail.com>
2017-03-03 21:38:23 -0800

    EOD commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,48 @@
 var models = require('../models');
 
 module.exports = {
+
+  headers: { 'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+  },
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      console.log('Wut?');
+      if (req.method === 'GET') {
+        console.log(req.url);
+        res.writeHead(27000, this.headers);
+        res.end('wut');
+      }
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      if (req.method === 'POST') {
+        res.writeHead(201, this.headers);
+        res.end('wut');
+      }
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      console.log('Wut?');
+      if (req.method === 'GET') {
+        console.log(req.url);
+        res.writeHead(2770, this.headers);
+        res.end('wut');
+      }
+    },
+    post: function (req, res) {
+      if (req.method === 'POST') {
+        res.writeHead(201, this.headers);
+        res.end('wut');
+      }
+    }
   }
 };
+
+//models.<messages /or users>.<get /or post>
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,55 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      db.query('SELECT * FROM messages', function(err, rows) {
+        if (err) {
+          throw err;
+        } else {
+          console.log('FUCK YEAH');
+          return rows;
+        }
+      });
+    }, // a function which produces all the messages
+    post: function (jsonObj) {
+      // var parsed = JSON.parse(jsonObj);
+      // {text: ... , username, roomname}
+      // console.log(parsed);
+      db.query('INSERT INTO message SET ?', jsonObj, function(err, res) {
+        if (err) {
+          throw err;
+        }
+        console.log('FUCK YEAH');
+        // done();
+      });
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      db.query('SELECT * FROM users', function(err, rows) {
+        if (err) {
+          throw err;
+        } else {
+          console.log('WUT?');
+          return rows;
+        }
+      });
+    },
+    post: function (jsonObj) {
+      // var parsed = JSON.parse(jsonObj);
+      // {text: ... , username, roomname}
+      // console.log(parsed);
+      db.query('INSERT INTO users SET ?', jsonObj, function(err, res) {
+        if (err) {
+          throw err;
+        }
+        console.log('FUCK YEAH');
+        // done();
+      });
+    }
   }
 };
 
